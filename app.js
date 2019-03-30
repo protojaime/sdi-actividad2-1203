4ee2043ab1af899553b11b059f6362b8d3f6fc75
protojaime <33036535+protojaime@users.noreply.github.com>
2019-03-30 13:13:58 +0100

    “SDI-NodeJS-8.5,6, 7-Registro de usuario, autenticación y uso de sesión”

--- app.js
+++ app.js
@@ -2,12 +2,17 @@
 var express = require('express');
 var app = express();
 
+var expressSession = require('express-session');
+app.use(expressSession({
+    secret: 'abcdefg',
+    resave: true,
+    saveUninitialized: true
+}));
+var crypto = require('crypto');
 var swig = require('swig');
 var fileUpload = require('express-fileupload');
 app.use(fileUpload());
 var mongo = require('mongodb');
-
-
 
 var bodyParser = require('body-parser');
 //meter body parser dentro de express
@@ -22,10 +27,8 @@ gestorBD.init(app,mongo);
 app.set('port', 8081);
 //app.set('db', 'mongodb://admin:sdi1203@tiendamusica-shard-00-00-96snv.mongodb.net:27017,tiendamusica-shard-00-01-96snv.mongodb.net:27017,tiendamusica-shard-00-02-96snv.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true');
 app.set('db', 'mongodb://localhost:27017/uomusic');
-
-
-
-
+app.set('clave','abcdefg');
+app.set('crypto',crypto);
 //antes de mirar controladores mirar public lo primero
 app.use(express.static('public'));
 
@@ -36,4 +39,4 @@ require("./routes/rcanciones.js")(app, swig,gestorBD); // (app, param1, param2, 
 // lanzar el servidor
 app.listen(app.get('port'), function () {
     console.log("Servidor activo");
-})
+});
