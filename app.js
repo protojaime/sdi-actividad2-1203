ae84fc71535ad09774bc31708f829626b07f90fe
protojaime <33036535+protojaime@users.noreply.github.com>
2019-06-13 16:59:29 +0200

    SDI-NodeJS-10.4-SW Cliente jQuery + AJAX (Ampliaciones)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-//Modulos
+// Módulos
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -17,7 +18,6 @@ app.set('jwt', jwt);
 var fs = require('fs');
 var https = require('https');
 
-
 var expressSession = require('express-session');
 app.use(expressSession({
     secret: 'abcdefg',
@@ -25,28 +25,92 @@ app.use(expressSession({
     saveUninitialized: true
 }));
 
-var crypto = require('crypto');
+var expressSession = require('express-session');
+// routerUsuarioSession
+var routerUsuarioSession = express.Router();
+routerUsuarioSession.use(function(req, res, next) {
+    console.log("routerUsuarioSession");
+    if ( req.session.usuario ) {
+        // dejamos correr la petición
+        next();
+    } else {
+        console.log("va a : "+req.session.destino)
+        res.redirect("/identificarse");
+    }
+});
+//Aplicar routerUsuarioSession
+app.use("/canciones/agregar",routerUsuarioSession);
+app.use("/publicaciones",routerUsuarioSession);
+app.use("/cancion/comprar",routerUsuarioSession);
+app.use("/compras",routerUsuarioSession);
 
-var fileUpload = require('express-fileupload');
-app.use(fileUpload());
+//routerAudios
+var routerAudios = express.Router();
+routerAudios.use(function(req, res, next) {
+    console.log("routerAudios");
+    var path = require('path');
+    var idCancion = path.basename(req.originalUrl, '.mp3');
+    gestorBD.obtenerCanciones(
+        {id : mongo.ObjectID(idCancion) }, function (canciones) {
+            if(canciones[0].autor == req.session.usuario ){
+                next();
+            } else {
+                var criterio = {
+                    usuario : req.session.usuario,
+                    cancionId : mongo.ObjectID(idCancion)
+                };
 
-var swig = require('swig');
+                gestorBD.obtenerCompras(criterio ,function(compras){
+                    if (compras != null && compras.length > 0 ){
+                        next();
+                        return;
+                    } else {
+                        res.redirect("/tienda");
+                    }
+                });
+            }
+        })
+});
+//Aplicar routerAudios
+app.use("/audios/",routerAudios);
+
+//routerUsuarioAutor
+var routerUsuarioAutor = express.Router();
+routerUsuarioAutor.use(function(req, res, next) {
+    console.log("routerUsuarioAutor");
+    var path = require('path');
+    var id = path.basename(req.originalUrl);
+// Cuidado porque req.params no funciona
+// en el router si los params van en la URL.
+    gestorBD.obtenerCanciones(
+        {_id: mongo.ObjectID(id) }, function (canciones) {
+            if(canciones[0].autor == req.session.usuario ){
+                next();
+            } else {
+                res.redirect("/tienda");
+            }
+        })
+});
+//Aplicar routerUsuarioAutor
+app.use("/cancion/modificar",routerUsuarioAutor);
+app.use("/cancion/eliminar",routerUsuarioAutor);
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // routerUsuarioToken
 var routerUsuarioToken = express.Router();
-routerUsuarioToken.use(function (req, res, next) {
+routerUsuarioToken.use(function(req, res, next) {
     // obtener el token, vía headers (opcionalmente GET y/o POST).
     var token = req.headers['token'] || req.body.token || req.query.token;
     if (token != null) {
-// verificar el token
-        jwt.verify(token, 'secreto', function (err, infoToken) {
-            if (err || (Date.now() / 1000 - infoToken.tiempo) > 240) {
+        // verificar el token
+        jwt.verify(token, 'secreto', function(err, infoToken) {
+            if (err || (Date.now()/1000 - infoToken.tiempo) > 240 ){
                 res.status(403); // Forbidden
                 res.json({
-                    acceso: false,
+                    acceso : false,
                     error: 'Token invalido o caducado'
                 });
                 // También podríamos comprobar que intoToken.usuario existe
@@ -62,7 +126,7 @@ routerUsuarioToken.use(function (req, res, next) {
     } else {
         res.status(403); // Forbidden
         res.json({
-            acceso: false,
+            acceso : false,
             mensaje: 'No hay Token'
         });
     }
@@ -70,78 +134,17 @@ routerUsuarioToken.use(function (req, res, next) {
 // Aplicar routerUsuarioToken
 app.use('/api/cancion', routerUsuarioToken);
 
-// routerUsuarioSession
-var routerUsuarioSession = express.Router();
-routerUsuarioSession.use(function (req, res, next) {
-    console.log("routerUsuarioSession");
-    if (req.session.usuario) {
-        // dejamos correr la petición
-        next();
-    } else {
-        console.log("va a : " + req.session.destino)
-        res.redirect("/identificarse");
-    }
-});
-//Aplicar routerUsuarioSession
-app.use("/canciones/agregar", routerUsuarioSession);
-app.use("/publicaciones", routerUsuarioSession);
+var swig = require('swig');
 
-//routerUsuarioAutor
-var routerUsuarioAutor = express.Router();
-routerUsuarioAutor.use(function (req, res, next) {
-    console.log("routerUsuarioAutor");
-    var path = require('path');
-    var id = path.basename(req.originalUrl);
-// Cuidado porque req.params no funciona
-// en el router si los params van en la URL.
-    gestorBD.obtenerCanciones(
-        {_id: mongo.ObjectID(id)}, function (canciones) {
-            console.log(canciones[0]);
-            if (canciones[0].autor == req.session.usuario) {
-                next();
-            } else {
-                var criterio = {
-                    usuario: req.session.usuario,
-                    cancionId: mongo.ObjectID(idCancion)
-                };
-
-                gestorBD.obtenerCompras(criterio, function (compras) {
-                    if (compras != null && compras.length > 0) {
-                        next();
-                    } else {
-                        res.redirect("/tienda");
-                    }
-                });
-            }
-        })
-});
-//Aplicar routerUsuarioAutor
-app.use("/cancion/modificar", routerUsuarioAutor);
-app.use("/cancion/eliminar", routerUsuarioAutor);
-app.use("/cancion/comprar", routerUsuarioSession);
-app.use("/compras", routerUsuarioSession);
-
-
-//routerAudios
-var routerAudios = express.Router();
-routerAudios.use(function (req, res, next) {
-    console.log("routerAudios");
-    var path = require('path');
-    var idCancion = path.basename(req.originalUrl, '.mp3');
-    gestorBD.obtenerCanciones(
-        {"_id": mongo.ObjectID(idCancion)}, function (canciones) {
-            if (req.session.usuario && canciones[0].autor == req.session.usuario) {
-                next();
-            } else {
-                res.redirect("/tienda");
-            }
-        })
-});
-//Aplicar routerAudios
-app.use("/audios/", routerAudios);
-
-app.use(express.static('public'));
 var mongo = require('mongodb');
+
+var fileUpload = require('express-fileupload');
+app.use(fileUpload());
+
+var crypto = require('crypto');
+
+var gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 // Variables
 app.set('port', 8081);
@@ -151,26 +154,27 @@ app.set('clave','abcdefg');
 app.set('crypto',crypto);
 
 
-var gestorBD = require("./modules/gestorBD.js");
-gestorBD.init(app, mongo);
-
-require("./routes/rusuarios")(app, swig, gestorBD);
-require("./routes/rcanciones.js")(app, swig, gestorBD);
+//Rutas/controladores por lógica
+require("./routes/rusuarios.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app, swig, gestorBD); // (app, param1, param2, etc.)
+require("./routes/rapicanciones.js")(app, gestorBD);
 
 app.get('/', function (req, res) {
     res.redirect('/tienda');
-});
+})
 
-app.use(function (err, req, res, next) {
+app.use( function (err, req, res, next ) {
     console.log("Error producido: " + err); //we log the error in our db
-    if (!res.headersSent) {
+    if (! res.headersSent) {
         res.status(400);
         res.send("Recurso no disponible");
     }
 });
 
-
-// lanzarel servidor
-app.listen(app.get('port'), function () {
+// lanzar el servidor
+https.createServer({
+    key: fs.readFileSync('certificates/alice.key'),
+    cert: fs.readFileSync('certificates/alice.crt')
+}, app).listen(app.get('port'), function() {
     console.log("Servidor activo");
 });
